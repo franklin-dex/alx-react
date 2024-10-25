9ea07ba91c373b4127435d0a7e142ac8782d65e5
franklin-dex <odinakachukwufranklin0@gmail.com>
2024-10-25 18:23:38 +0100

    Initial gh-pages commit

---
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Notifications.css";
+import close_icon from "./close-icon.png";
+import { getLatestNotification } from "./utils.js";
+
+function Notifications() {
+  return (
+    <div className="Notifications">
+      <button
+        style={{
+          color: "#3a3a3a",
+          fontWeight: "bold",
+          background: "none",
+          border: "none",
+          fontSize: "15px",
+          position: "absolute",
+          right: "20px",
+          top: "20px",
+          cursor: "pointer",
+        }}
+        aria-label="close"
+        onClick={() => console.log("Close button has been clicked")}
+      >
+        <img src={close_icon} alt="close" width="15px" height="15px" />
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li
+          data-priority="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Notifications;
---
+++ 0x01-react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import header from "./holberton-logo.jpg";
+import { getFullYear, getFooterCopy } from "./utils";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={header} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email" />
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password" />
+          <button>OK</button>
+        </form>
+      </div>
+      <div className="App-footer">
+        Copyright {getFullYear()} - {getFooterCopy()}
+      </div>
+    </div>
+  );
+}
+
+export default App;
