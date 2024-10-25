f0cb9d0d2d0c77d09f654cf061ab8200cd1a3aa3
franklin-dex <odinakachukwufranklin0@gmail.com>
2024-10-25 13:39:02 +0100

    Enzyme install

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-18'; // Ensure the correct adapter is imported
+
+configure({ adapter: new Adapter() });
